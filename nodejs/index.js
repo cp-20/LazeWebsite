1e22eaa3ba39a1e7b6d5eaf95e428fac0aeccc3e
mario-hsp <47262658+mario-hsp@users.noreply.github.com>
2021-03-15 14:21:10 +0900

    見切れ修正

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { exec } = require('node:child_process');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 80
+const port = 80;
 
-app.use('/', express.static('/home/pi/compilerserver/Compiler/'))
+app.use('/', express.static('/home/pi/compilerserver/Compiler/'));
 app.get('/', (req, res) => {
   res.sendFile('/home/pi/compilerserver/Compiler/index.html');
-})
+});
 
 app.listen(port, () => {
-  console.log(`Compiler Server listening at http://rootlang.ddns.net`)
-})
+  console.log(`Compiler Server listening at http://rootlang.ddns.net`);
+});
+
+io.on('connection', socket => {
+  socket.on('compile', async input => {
+    // コンパイル
+    exec('')
+
+    // 出力
+  })
+});
