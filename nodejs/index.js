438999ae5e6283ff1f5fecacf785695dec188c8b
coder6583 <thedude6583@gmail.com>
2021-03-16 22:46:22 +0900

    temp

--- nodejs/index.js
+++ nodejs/index.js
@@ -25,6 +25,7 @@ io.sockets.on('connection', socket => {
     exec('echo \"' + input.value + '\" > ' + input.filename);
     exec('./compiler ' + input.filename, (err, stdout, stderr) =>
     {
+	console.log(stdout);
       // 出力
         if(err) {
           socket.emit('output', {
