7464cbfa467b97290c32f863d1a1cfa9e104d1c3
coder6583 <thedude6583@gmail.com>
2021-03-16 13:57:09 +0900

    temp

--- nodejs/index.js
+++ nodejs/index.js
@@ -19,7 +19,7 @@ io.sockets.on('connection', socket => {
   var address = socket.handshake.address;
   console.log('New connection from ' + JSON.stringify(address) + socket.id);
   //defaultはguestとして入る
-  users.set(socket.id, "guest");
+  users.set(socket.id, "guesta");
   socket.on('compile', async input => {
     // コンパイル
     exec('echo \"' + input + '\" > test.lang');
