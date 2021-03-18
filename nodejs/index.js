7eb8829b94c9260fe73ba6b85beccea9f9424fa2
curvingchicken <thedude6583@gmail.com>
2021-03-18 13:14:32 +0900

    1

--- nodejs/index.js
+++ nodejs/index.js
@@ -38,7 +38,7 @@ async function readDirectory(path, socket, result, callback)
           // let val = await readDirectory(path + '/' + element.name, socket, {type: 'folder', name: element.name, folder: []});
           // console.log(await readDirectory(path + '/' + element.name, socket, {type: 'folder', name: element.name, folder: []}));
           const temp = await readDirectory(path + '/' + element.name, socket, {type: 'folder', name: element.name, folder: []}, (val) => {
-            console.log(val);
+            console.log(val, 'a');
             folders.set(element.name, val);
             return (val);
           });
