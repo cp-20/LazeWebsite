d1df6d991f183dab8b3549b617728d4dd06cda01
curvingchicken <thedude6583@gmail.com>
2021-03-20 15:50:50 +0900

    ts

--- server/nodejs/server.ts
+++ server/nodejs/server.ts
@@ -43,8 +43,8 @@ app.get('/admin', (req: express.Request, res: express.Response) => {
     res.sendFile('client/admin.html', {root: rootDir});
 })
 
-let users = new Map();
-let usersDirectory = new Map();
+let users: Map<string, string> = new Map();
+let usersDirectory: Map<string, string> = new Map();
 
 async function readDirectory(path: string, socket: any, result: dirObject)
 {
@@ -58,7 +58,6 @@ async function readDirectory(path: string, socket: any, result: dirObject)
     }
     else
     {
-
       let files = new Map();
       let folders = new Map();
       content.forEach(async element => {
@@ -86,3 +85,121 @@ async function readDirectory(path: string, socket: any, result: dirObject)
     return result;
   });
 }
+
+io.sockets.on('connection', (socket:any) => {
+    var address = socket.handshake.address;
+    console.log('New connection from ' + JSON.stringify(address) + socket.id);
+    //defaultはguestとして入る
+    users.set(socket.id, "guest");
+    fs.mkdir(accountsDir + 'guest/' + socket.id, (err) => {
+      if(err)
+      {
+        console.log('could not create ' + accountsDir + 'guest/' + socket.id);
+      }
+    });
+    usersDirectory.set(socket.id, accountsDir + 'guest/' + socket.id);
+    socket.on('compile', async input => {
+      // コンパイル
+      exec('echo \"' + input.value + '\" > ' + usersDirectory.get(socket.id) + '/' + input.filename);
+      exec('./compiler ' + input.filename + ' ' + usersDirectory.get(socket.id) + '/', (err, stdout, stderr) =>
+      {
+        // 出力
+          console.log(err, stdout, stderr);
+          if(err) {
+            socket.emit('output', {
+              value: stderr,
+              style: 'err'
+            });
+          }else {
+            socket.emit('output', {
+              value: stdout,
+              style: 'log'
+            })
+          }
+          return;
+      }
+      );
+      exec('sudo rm -f ' + input.filename + ' .' + input.filename);
+  
+    })
+    socket.on('save', async input => {
+      //ファイルにセーブ
+      if(users.get(socket.id) == 'guest')
+      {
+        socket.emit('saved', {
+          value: 'If you want to save a file, please create an account.',
+          style: 'err',
+          success: false
+        })
+      }
+      else{
+        exec('echo \"' + input.value + '\" > ' + usersDirectory.get(socket.id) + '/' + input.projectName + '/' + input.filename, (err, stdout, stderr) => {
+          if(err) {
+            socket.emit('saved', {
+              value: stderr + ' : Save not complete.',
+              style : 'err',
+              success: false
+            })
+          }
+          else
+          {
+            socket.emit('saved', {
+              value: 'Save complete.',
+              style: 'info',
+              success: true
+            })
+          }
+          return;
+        });
+      };
+    });
+    //loginシステム
+    socket.on('login', async input => 
+    {
+      //usersのvalueをアカウント名にする
+      users.set(socket.id, input.accountName);
+      usersDirectory.set(socket.id, accountsDir + input.accountName);
+    });
+    //すでに作られたProjectをロードする
+    socket.on('loadProject', async input => 
+    {
+      let result = {type: 'folder', name: input.projectName, folder: []};
+      // console.log(readDirectory(usersDirectory.get(socket.id) + '/' + input.projectName, socket, result));
+      readDirectory(usersDirectory.get(socket.id) + '/' + input.projectName, socket, result).then((val) => {
+        socket.emit('loadedProject', {
+          value: val,
+          style: 'log'
+        });
+      });
+    });
+    //Projectを作る
+    socket.on('createProject', async input => {
+      fs.mkdir(usersDirectory.get(socket.id) + '/' + input.projectName, (err) => {
+        if(err)
+        {
+          socket.emit('createdProject', {
+            value: 'Could not create project '+ input.projectName,
+            style: 'err'
+          })
+        }
+        else
+        {
+          socket.emit('createdProject', {
+            value: 'Created project ' + input.projectName,
+            style: 'log'
+          })
+        }
+      });
+    })
+    //disconnectしたとき
+    socket.on('disconnect', () => {
+      console.log("a");
+      if(users.get(socket.id) == 'guest')
+      {
+        fs.rmdir(usersDirectory.get(socket.id), (err) => {
+          console.log(usersDirectory.get(socket.id));
+        });        
+      }
+    })
+  });
+  
