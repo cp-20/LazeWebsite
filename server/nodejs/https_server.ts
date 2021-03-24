8c7b80284b17da177cb87d239530744aaf1fb298
curvingchicken <thedude6583@gmail.com>
2021-03-24 17:36:57 +0900

    bug fix

--- server/nodejs/https_server.ts
+++ server/nodejs/https_server.ts
@@ -308,6 +308,7 @@ io.sockets.on('connection', (socket:any) => {
     });
     usersDirectory.set(socket.id, accountsDir + 'guest/' + socket.id);
     console.log(socket.request.session);
+    console.log(socket.request.user);
     socket.on('compile', async (input: compileData) => {
       // コンパイル
       exec('echo \"' + input.value + '\" > ' + usersDirectory.get(socket.id) + '/' + input.filename, (err: NodeJS.ErrnoException| null, stdout: Stream, stderr: Stream) => {
