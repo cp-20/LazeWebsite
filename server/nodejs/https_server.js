9c70e73380b70af5c83b780e9ab28515279a15c3
curvingchicken <thedude6583@gmail.com>
2021-05-29 16:08:57 +0900

    wasmPath fix

--- server/nodejs/https_server.js
+++ server/nodejs/https_server.js
@@ -318,7 +318,7 @@ app.get('/avatar/id', function (req, res) {
     });
 });
 app.get('/getwasm', function (req, res) {
-    var wasmPath = path.resolve("" + accountsDir + req.query.account, "." + req.query.filename + ".wasm");
+    var wasmPath = "" + accountsDir + req.query.account + "/." + ;
     fs_1.default.access(wasmPath, function (err) {
         if (!err) {
             res.sendFile(wasmPath);
--- server/nodejs/https_server.ts
+++ server/nodejs/https_server.ts
@@ -306,7 +306,7 @@ app.get('/avatar/id', (req: express.Request, res: express.Response) => {
 	})
 })
 app.get('/getwasm', (req: express.Request, res: express.Response) => {
-  let wasmPath = path.resolve(`${accountsDir}${req.query.account}`, `.${req.query.filename}.wasm`);
+  let wasmPath = `${accountsDir}${req.query.account}/.${req.query.filename}.wasm`;
   fs.access(wasmPath, (err) => {
     if(!err)
     {
